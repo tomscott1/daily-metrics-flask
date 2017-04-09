19f5961e3997d9170df47d6e1a1356c07848f02d
Tom Scott <tomscott85@gmail.com>
2017-04-09 12:11:02 +1000

    include header template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Daily Metrics'
+    user = {'name': 'Billy'}  # fake user
+    metrics = [
+        {
+            'name': 'Skipping',
+            'text': 'daily skipping record'
+        },
+        {
+            'name': 'Chinese',
+            'text': 'daily chinese practise'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, metrics=metrics)

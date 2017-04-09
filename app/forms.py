38af8daec57b69dc1ea12969db47a511fa8cb5dc
Tom Scott <tomscott85@gmail.com>
2017-04-09 15:42:41 +1000

    remove openid add errors and flash to next page

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,6 @@ from wtforms.validators import DataRequired, Email
 
 
 class LoginForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
     email = StringField('email', validators=[DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired()])
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
@@ -25,7 +25,7 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenId="%s", remember_me="%s"' %
-              (form.openid.data, str(form.remember_me.data)))
+        flash('Login requested for remember_me="%s", email="%s", password="%s"' %
+              (str(form.remember_me.data), form.email.data, form.password.data))
         return redirect('/index')
     return render_template('login.html', title='Sign in', form=form)

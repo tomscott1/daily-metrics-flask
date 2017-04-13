fc3665f6de335a3e1713ec032b901197ba54e37c
Tom Scott <tomscott85@gmail.com>
2017-04-14 08:12:39 +1000

    add models and migrations

--- app/models.py
+++ app/models.py
@@ -7,3 +7,25 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
+
+class Metric(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), index=True)
+    name = db.Column(db.String(64))
+    # metric_type = db.Column(db.Integer)
+    max_val = db.Column(db.Integer)
+    min_val = db.Column(db.Integer)
+    increment = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Post %r>' % (self.name)
+
+
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    metric_id = db.Column(db.Integer, db.ForeignKey('Metric.id'), index=True)
+    value = db.Column(db.Integer)
+    date = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<Record %r>' % (self.value)
